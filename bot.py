82a4904f82f1b6532326416c17633ed73993b9c9
russell_h <russellhaering@gmail.com>
2009-06-23 20:33:23 -0700

    A semi-working @ansr8r Twitter Bot. *Lots* of work to do.

--- bot.py
+++ bot.py
@@ -1,33 +1,92 @@
 #!/usr/bin/env python
-import os
-import sys
+import sys, os, glob, cPickle
 
 sys.path.append(os.path.join(sys.path[0], 'twitty-twister', 'lib'))
 
 from twisted.internet import reactor, protocol, defer, task
 from twisted.python import log
 
+import aiml
 import twitter
 import settings
 
+
 class TwitterBot:
     def __init__(self):
-        print "Initializing..."
+        self.status = {}
+        print "Initializing AIML Bot..."
+        self.k = aiml.Kernel()
+        print "Loading Brain..."
+        if os.path.isfile(settings.BRAINFILE):
+            self.k.loadBrain(settings.BRAINFILE)
+        else:
+            self._loadBrainFromAIML()
+        print "Loading Sessions..."
+        self._loadSessions()
+        print "Loading Previous Status Data..."
+        self._loadBotStatusData()
+        reactor.addSystemEventTrigger('before', 'shutdown', self._onShutDown)
+        print "Connecting to Twitter..."
         self.feed = twitter.TwitterFeed(settings.USERNAME, settings.PASSWORD)
-        print "Logged In"
+        print "Fetching Account Data..."
         self.feed.show_user(settings.USERNAME).addCallback(self.onLogin).addErrback(lambda fail: fail.printTraceback())
+
+    def _onShutDown(self):
+        self.k.saveBrain(settings.BRAINFILE)
+        print "Brain Saved"
+        sessionFile = open(settings.SESSNFILE, 'w')
+        cPickle.dump(self.k.getSessionData(), sessionFile)
+        sessionFile.close()
+        print "Sessions Saved"
+        self._saveBotStatusData()
+
+
+    def _loadBrainFromAIML(self):
+        for file in glob.glob(os.path.join('standard', '*.aiml')):
+            self.k.learn(file)
+
+    def _loadSessions(self):
+        if os.path.isfile(settings.SESSNFILE):
+            sessionfile = open(settings.SESSNFILE, 'r')
+            sessions = cPickle.load(sessionfile)
+            for user, data in sessions.items():
+                for pred, value in data.items():
+                    self.k.setPredicate(pred, value, user)
+
+    def _saveBotStatusData(self):
+        statusFile = open(settings.STATUSFILE, 'w')
+        cPickle.dump(self.status, statusFile)
+
+    def _loadBotStatusData(self):
+        if os.path.isfile(settings.STATUSFILE):
+            statusFile = open(settings.STATUSFILE, 'r')
+            self.status = cPickle.load(statusFile)
+
+    def onFollowStreamClose(self):
+        """Called when the follow stream connection is lost, reopens the
+            connection recursively"""
+        self.feed.follow(self.onMention, user.id).addCallback(self.onFollowStreamClose).addErrback(log.err)
 
     def onLogin(self, user):
         self.id = user.id
-        self.feed.follow(self.onFollowEvent, user.id).addErrback(log.err)
+        # Deal with (latest 20) Mentions Since Shutdown
+        try:
+            self.feed.mentions(self.onMention, params={'since_id': unicode(self.status['lastMentionRepliedTo'])}) 
+        except KeyError:
+            print "No previous post data available, skipping"
+            pass
+        # Begin Following Self
+        self.feed.follow(self.onMention, user.id).addCallback(self.onFollowStreamClose).addErrback(log.err)
 
-    def onFollowEvent(self, entry):
-        #print entry.text
+    def onMention(self, entry):
         if entry.user.screen_name == settings.USERNAME:
             return
         else:
-            reply = entry.text.replace('@' + settings.USERNAME, 'RT @' + entry.user.screen_name, 1)
-            self.feed.update(reply).addErrback(lambda fail: fail.printTraceback())
+            input = entry.text.replace('@' + settings.USERNAME, '', 1)
+            reply = '@%s %s' % (entry.user.screen_name, self.k.respond(input, entry.user.screen_name))
+            print "Responding to", entry.id, ":", input, "with:", reply
+            self.feed.update(reply, in_reply_to=entry.id).addErrback(lambda fail: fail.printTraceback())
+            self.status['lastMentionRepliedTo'] = entry.id
 
 
 if __name__ == "__main__":
